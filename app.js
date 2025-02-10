d111d7dfa5c69bf31d1abc124397956bee5c8f2b
imannolM <imonroydj@gmail.com>
2025-02-10 12:38:44 -0600

    Implementamos la función actualizarListaAmigos para agregar los nombres a la lista del HTML

--- app.js
+++ app.js
@@ -13,8 +13,21 @@ function agregarAmigo() {
     }
     console.log(amigos);
     limpiarCampo();
+    actualizarListaAmigos();
 }
 
 function limpiarCampo() {
     document.querySelector('#amigo').value= '';
+}
+
+function actualizarListaAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = "";
+
+    for (let index = 0; index < amigos.length; index++) {
+        let nuevoElemento = document.createElement('li');
+        nuevoElemento.textContent = amigos[index];
+        listaAmigos.appendChild(nuevoElemento); 
+    }
+    console.log(listaAmigos)
 }
