834f3d37213e9fdf32bd4791ed9fbe5a0f7f6bcb
imannolM <imonroydj@gmail.com>
2025-02-10 12:02:02 -0600

    Realizamos la función agregarAmigo y limpiarCampo en el JS

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 //variables
+let amigos = [];
 
+
+//funciones
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value;
+
+    if (nombreAmigo == '') {
+        alert('Por favor, inserte un nombre');
+    } else {
+        amigos.push(nombreAmigo);
+    }
+    console.log(amigos);
+    limpiarCampo();
+}
+
+function limpiarCampo() {
+    document.querySelector('#amigo').value= '';
+}
